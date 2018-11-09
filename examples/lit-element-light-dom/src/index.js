71743c8c38a91d04ded2fe755a49fd10b95cfd8a
Luiz Américo <luizamericop@gmail.com>
2018-11-09 15:56:58 -0300

    Add lit-element light example

---
+++ examples/lit-element-light-dom/src/index.js
@@ -0,0 +1,152 @@
+import { fromProp } from 'wc-context/lit-element'
+import { html } from '@polymer/lit-element'
+import { styles } from './styles'
+import { Component } from './component'
+
+class ThemeProvider extends Component {
+  static get properties () {
+    return {
+      theme: { type: String },
+      alttheme: { type: String },
+      activeTheme: { type: String }
+    }
+  }
+
+  constructor () {
+    super()
+    this.childContext = {
+      theme: fromProp('activeTheme')
+    }
+
+    this.toggleTheme = () => {
+      const primaryTheme = this.theme || 'light'
+      const altTheme = this.alttheme || 'dark'
+      this.activeTheme =
+        this.activeTheme === primaryTheme ? altTheme : primaryTheme
+    }
+  }
+
+  connectedCallback () {
+    super.connectedCallback()
+    this.activeTheme = this.theme || 'light'
+  }
+
+  render () {
+    return html`
+      <button @click=${this.toggleTheme}>toggle theme</button>
+      <slot/>
+    `
+  }
+}
+
+class ThemeConsumer extends Component {
+  static get observedContexts () {
+    return ['theme']
+  }
+
+  contextChangedCallback (name, oldValue, value) {
+    console.log(
+      this.constructor.name,
+      `context "${name}" changed from "${oldValue}" to "${value}"`
+    )
+    this.requestUpdate()
+  }
+
+  render () {
+    return html`<div style=${styles[this.context.theme]}>${
+      this.context.theme
+    }</div>`
+  }
+}
+
+class TitleProvider extends Component {
+  static get properties () {
+    return {
+      value: { type: String }
+    }
+  }
+
+  constructor () {
+    super()
+    this.childContext = {
+      title: fromProp('value')
+    }
+  }
+
+  render () {
+    return html`<slot/>`
+  }
+}
+
+class TitleThemeConsumer extends Component {
+  static get observedContexts () {
+    return ['title', 'theme']
+  }
+
+  contextChangedCallback (name, oldValue, value) {
+    console.log(
+      this.constructor.name,
+      `context "${name}" changed from "${oldValue}" to "${value}"`
+    )
+    this.requestUpdate()
+  }
+
+  render () {
+    return html`
+    <div>${this.context.title}</div>
+    <div style=${styles[this.context.theme]}>${this.context.theme}</div>
+    `
+  }
+}
+
+class App extends Component {
+  static get properties () {
+    return {
+      state: { type: Object }
+    }
+  }
+  constructor () {
+    super()
+    this.state = { title: 'one title' }
+    this.toggleTitle = () => {
+      this.state = {
+        ...this.state,
+        ...{
+          title:
+            this.state.title === 'one title' ? 'another title' : 'one title'
+        }
+      }
+    }
+  }
+
+  render () {
+    return html`
+      <div>
+        <theme-provider>
+          <theme-consumer></theme-consumer>
+        </theme-provider>
+        <theme-provider>
+          <theme-provider theme="blue" alttheme="yellow">
+            <theme-consumer></theme-consumer>
+          </theme-provider>
+        </theme-provider>
+        <theme-provider>
+          <title-provider value=${this.state.title}>
+            <titletheme-consumer></titletheme-consumer>
+          </title-provider>
+        </theme-provider>        
+        <button @click=${this.toggleTitle}>Toggle title</button>
+      </div>
+    `
+  }
+}
+
+customElements.define('theme-provider', ThemeProvider)
+customElements.define('theme-consumer', ThemeConsumer)
+customElements.define('title-provider', TitleProvider)
+customElements.define('titletheme-consumer', TitleThemeConsumer)
+customElements.define('context-example', App)
+
+const appEl = document.createElement('context-example')
+
+document.body.appendChild(appEl)
