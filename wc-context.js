b68ab0fe023889ed37ee90da45eaab88304fb065
Luiz Américo <camara_luiz@yahoo.com.br>
2021-12-26 21:57:05 -0300

    Simplify component mixin

--- wc-context.js
+++ wc-context.js
@@ -5,12 +5,29 @@ import {
   updateContext,
 } from './core.js'
 
-const initializedElements = new WeakSet()
+function getWithConfig(provider, config) {
+  const property = typeof config === 'string' ? config : config.property
+  if (property) return provider[property]
+  return config.value
+}
 
 const withContext = (Base) => {
   return class extends Base {
+    constructor() {
+      super()
+      const providedContexts = this.constructor.providedContexts
+      if (providedContexts) {
+        Object.keys(providedContexts).forEach((name) => {
+          const config = providedContexts[name]
+
+          registerContext(this, name, config, getWithConfig)
+        })
+      }
+    }
+
     updateProvidedContext(name, value) {
-      updateContext(this, name, value)
+      const config = value !== undefined ? { value } : undefined
+      updateContext(this, name, config)
     }
 
     connectedCallback() {
@@ -18,24 +35,6 @@ const withContext = (Base) => {
       const observedContexts = this.constructor.observedContexts
       if (observedContexts) {
         observedContexts.forEach((context) => observeContext(this, context))
-      }
-
-      if (!initializedElements.has(this)) {
-        const providedContexts = this.constructor.providedContexts
-        if (providedContexts) {
-          Object.keys(providedContexts).forEach((name) => {
-            const config = providedContexts[name]
-            const property =
-              typeof config === 'string' ? config : config.property
-
-            registerContext(
-              this,
-              name,
-              property ? this[property] : config.value
-            )
-          })
-        }
-        initializedElements.add(this)
       }
     }
 
