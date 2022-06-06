81252ea92abac55b98ad79c6dd9ea56f6f9365d4
Luiz Américo <camara_luiz@yahoo.com.br>
2022-06-06 21:02:03 +0000

    Implement ContextConsumer controller

---
+++ test/consumer.spec.js
@@ -0,0 +1,76 @@
+import { registerContext, updateContext } from '../core.js'
+import { ContextConsumer } from '../consumer.js'
+import { LitElement } from 'lit'
+
+const ComponentWithConsumer = class extends LitElement {}
+
+customElements.define('lit-controller-consumer', ComponentWithConsumer)
+
+describe('ContextConsumer', () => {
+  let rootEl
+  let grandfatherEl
+  let parentEl
+  let childEl
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div id="root">
+        <div id="grandfather">
+          <div id="parent">
+            
+          </div>
+          <div id="parent2">
+            <div id="child2"></div>
+          </div>
+        </div>       
+      </div>
+    `
+    rootEl = document.getElementById('root')
+    grandfatherEl = document.getElementById('grandfather')
+    parentEl = document.getElementById('parent')
+    childEl = document.getElementById('child')
+  })
+
+  it('should consume parent context when connected', () => {
+    registerContext(grandfatherEl, 'key', 'value')
+    const component = new ComponentWithConsumer()
+    const ctxConsumer = new ContextConsumer(component, 'key')
+    parentEl.appendChild(component)
+    expect(ctxConsumer.value).toBe('value')
+  })
+
+  it('should update its value when context changes', () => {
+    registerContext(grandfatherEl, 'key', 'value')
+    const component = new ComponentWithConsumer()
+    const ctxConsumer = new ContextConsumer(component, 'key')
+    parentEl.appendChild(component)
+    updateContext(grandfatherEl, 'key', 'value2')
+    expect(ctxConsumer.value).toBe('value2')
+  })
+
+  it('should call callback param when context changes', async () => {
+    registerContext(grandfatherEl, 'key', 'value')
+    const component = new ComponentWithConsumer()
+    const callback = jest.fn()
+    const ctxConsumer = new ContextConsumer(component, 'key', callback)
+    parentEl.appendChild(component)
+    expect(callback).toHaveBeenCalledTimes(1)
+    expect(callback).toHaveBeenCalledWith('value')
+    callback.mockClear()
+    await component.updateComplete
+    updateContext(grandfatherEl, 'key', 'value2')
+    expect(callback).toHaveBeenCalledTimes(1)
+    expect(callback).toHaveBeenCalledWith('value2')
+  })
+
+  it('should call host requestUpdate when context changes and callback is ommited', async () => {
+    registerContext(grandfatherEl, 'key', 'value')
+    const component = new ComponentWithConsumer()
+    const ctxConsumer = new ContextConsumer(component, 'key')
+    parentEl.appendChild(component)
+    await component.updateComplete
+    const updateFn = jest.spyOn(component, 'requestUpdate')
+    updateContext(grandfatherEl, 'key', 'value2')
+    expect(updateFn).toHaveBeenCalledTimes(1)
+  })
+})
