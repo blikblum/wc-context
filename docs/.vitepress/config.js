40ba975f21dc79086e3798f49b03c8ad9b37d7db
Luiz Américo <camara_luiz@yahoo.com.br>
2023-02-03 14:13:15 +0000

    Add vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,23 @@
+export default {
+  title: 'wc-context',
+  description: 'Context for web components',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Introduction',
+        items: [
+          { text: 'Features', link: '/index' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ],
+      },
+      {
+        text: 'Guides',
+        items: [
+          { text: 'Lit integration', link: '/lit' },
+          { text: 'Custom elements', link: '/custom-elenents' },
+          { text: 'Prebuilt components', link: '/prebuilt' },
+        ],
+      },
+    ],
+  },
+}
